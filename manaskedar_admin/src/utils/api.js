8b110a7b2b6b3ffa6e99744733362d76aecf290d
Rohit Rana <rkrana6631@gmail.com>
2026-04-05 10:16:05 +0530

    update

--- manaskedar_admin/src/utils/api.js
+++ manaskedar_admin/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'http://187.127.143.43:5001/api',
 });
 
 // Add interceptor to attach token automatically
